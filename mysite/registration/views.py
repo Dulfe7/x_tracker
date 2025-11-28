a875d814755cac7c82e03032c48fb4d18600a2bc
Dulfe7 <108720297+Dulfe7@users.noreply.github.com>
2025-11-28 16:05:45 +0300

    Delete mysite directory

--- mysite/registration/views.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.views import LoginView
-from .mixins import UserDeviceMixin
-
-
-class CustomLoginView(LoginView, UserDeviceMixin):
-    def form_valid(self, form):
-        self.save_user_device_info(self.request)
-        return super().form_valid(form)
